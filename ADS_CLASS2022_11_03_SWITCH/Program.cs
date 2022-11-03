b0a8a965f04f972140fa7077a77b8aeb80b66f98
JessicaGalhardi <104734490+JessicaGalhardi@users.noreply.github.com>
2022-11-03 16:51:48 +0100

    Adicionar arquivos de projeto.

---
+++ ADS_CLASS2022_11_03_SWITCH/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+ class Program
+{ 
+
+    static void Main(string[] args)
+    {
+        try
+        {
+
+            Console.WriteLine("Please, enter with the name's car: ");
+            string? car = Console.ReadLine();
+
+            switch (car)
+            {
+                case "Ferrari":
+                    Console.WriteLine("We have two Ferrari's available for rent!");
+                    break;
+                case "Porsche":
+                    Console.WriteLine("We have five Porsche's available for rent!");
+                    break;
+                case "Corolla":
+                    Console.WriteLine("We have one Corolla available for rent!");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Car not found!");
+                    break;
+         
+            }
+
+            Console.ReadKey();
+
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error while running the application" + ex);
+
+        }
+    }
+}
